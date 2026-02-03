b24a254bb0c2fc3a91014fdabcecbf9815f89486
daavii-b <brit.davi122@gmail.com>
2026-02-03 20:13:34 -0300

    feat: :sparkles: refactor HTTPClient to use improved singleton pattern and update client configuration

--- src/config/http/http.py
+++ src/config/http/http.py
@@ -1,23 +1,17 @@
-from typing import Any, ClassVar, TypeVar, cast
+from typing import Any, Literal
 
-from httpx import AsyncClient
+from httpx import AsyncClient, Limits
 
-T = TypeVar("T")
-
-
-class SingletonMeta(type):
-    instances: ClassVar[dict[type, Any]] = {}
-
-    def __call__(cls: type[T], *args: object, **kwargs: object) -> T:
-        cls_meta = cast("SingletonMeta", cls)
-
-        if cls not in cls_meta.instances:
-            instance = super().__call__(*args, **kwargs)
-            cls_meta.instances[cls] = instance
-
-        return cls_meta.instances[cls]
+from shared.patterns.singleton import SingletonMeta
 
 
 class HTTPClient(metaclass=SingletonMeta):
-    def get_client(self, base_url: str) -> AsyncClient:
-        return AsyncClient(base_url=base_url)
+    def get_client(self, base_url: str | None = None) -> AsyncClient:
+        client_config: dict[Literal["limits", "base_url"], Any] = {
+            "limits": Limits(max_keepalive_connections=20, max_connections=100)
+        }
+
+        if base_url is not None:
+            client_config["base_url"] = base_url
+
+        return AsyncClient(**client_config)
